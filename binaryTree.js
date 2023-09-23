4bcc3981577e7887c6efff40f5e71307757a5bce
damiangohrh123 <damiangohrh@gmail.com>
2023-09-23 22:12:09 +0800

    Add CASE 2: Delete a node with 2 childs for (#deleteHelper)

--- binaryTree.js
+++ binaryTree.js
@@ -39,56 +39,101 @@ class Tree {
     // Recursive case
     if (data < currentNode.data) {
       currentNode.left = this.insert(data, currentNode.left);
-      console.log(currentNode);
     } else if (data > currentNode.data) {
       currentNode.right = this.insert(data, currentNode.right);
-      console.log(currentNode);
     }
     return currentNode;
   }
 
-  delete(data, currentNode = this.root) {
-    // If node is empty, return
-    if (currentNode === null) return;
+  delete(data, currentNode = this.root, previousNode = null) {
+    // Base case: If node is empty, return null
+    if (currentNode === null) return this.root;
 
+    // Recursive case: passing data, current node and previous node
     if (data < currentNode.data) {
-      console.log(currentNode);
-      currentNode.left = this.delete(data, currentNode.left)
+      currentNode.left = this.delete(data, currentNode.left, currentNode)
+      return currentNode;
     } else if (data > currentNode.data) {
-      console.log(currentNode);
-      currentNode.right = this.delete(data, currentNode.right);
+      currentNode.right = this.delete(data, currentNode.right, currentNode);
+      return currentNode;
     } else if (data === currentNode.data) {
-      this.#deleteHelper();
+      return this.#deleteHelper(currentNode, previousNode);
+    }
+  }
+
+  //Node deletion helper. Has 3 cases to help delete() work.
+  #deleteHelper(currentNode, previousNode) {
+    // CASE 1: Delete a leaf node
+    if (currentNode.left === null && currentNode.right === null) {
+      return currentNode = null;
+    }
+
+    // CASE 2: Delete a node with 2 childs
+    if (currentNode.left && currentNode.right) {
+     return this.#successor(currentNode);
+    }
+
+    // CASE 3: Delete a node with a single child
+    if (currentNode.left || currentNode.right) {
+
     }
   }
 
   /**
+   * In the case where the current node has 2 children (#deleteHelper CASE 3),
+   * this functions finds the minimum value in the right subtree of the current node.
+   */
+  #successor(currentNode) {
+
+    let rightSubtree = currentNode.right;
+    let previousNode = rightSubtree;
+
+    // Use inorder traversal to find minimum value in the right subtree of the current node.
+    while (rightSubtree.left !== null) {
+      previousNode = rightSubtree;
+      rightSubtree = rightSubtree.left;
+    }
+
+    // Set the 'deleted' node data to the successor data. Then, delete the successor node.
+    if (previousNode === rightSubtree && rightSubtree.left === null) {
+      currentNode.data = rightSubtree.data;
+      currentNode.right = null;
+      console.log(currentNode);
+      console.log(rightSubtree);
+      console.log(previousNode);
+    }
+
+    if (previousNode !== rightSubtree) {
+      currentNode.data = rightSubtree.data;
+      previousNode.left = null;
+    }
+    return currentNode;
+  }
+
+    /**
    * Console logs out the Binary Tree in a structured format to make it easier to visualize.
    * No idea how this code works. Will try to understand in the future.
    */
-  prettyPrint(node = this.root, prefix = "", isLeft = true) {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
-
-  //Node deletion helper. Has 3 cases to help delete() work.
-  #deleteHelper() {
-    
-  }
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
+      if (node === null) {
+        return;
+      }
+      if (node.right !== null) {
+        this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      }
+      console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+      if (node.left !== null) {
+        this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      }
+    };
 }
 
-const arrayData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+//const arrayData = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const arrayData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 const tree = new Tree(arrayData);
 
 // Tests
-//tree.insert(10);
-tree.prettyPrint();
+//tree.insert(3);
 tree.delete(9);
+tree.prettyPrint();
+console.log(tree);
